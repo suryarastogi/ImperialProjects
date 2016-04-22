9f2b7e6dac8bdedce640ea80bb9041a51c73c648
Suryansh Rastogi <suryansh@gmail.com>
2016-04-22 03:20:49 +0100

    Initial DRF entry

---
+++ API/models.py
@@ -0,0 +1,26 @@
+from __future__ import unicode_literals
+
+from django.db import models
+
+class BlockVizRequest(models.Model):
+	# When the query was made
+    created = models.DateTimeField(auto_now_add=True)
+    # The start block for the visualisation analysis
+    start = models.IntegerField()
+    # The end block 
+    end = models.IntegerField()
+    # Connectivity threshold for subcomponents
+    threshold = models.IntegerField(default=0)
+
+    #transactions = models.TextField()
+    # Path where the model is saved
+    path = models.TextField()
+    # Rendered/laid out
+    rendered = models.BooleanField(default=False)
+    
+    class Meta:
+        ordering = ('created',)
+
+class Subcomponent(models.Model):
+ 	request = models.ForeignKey(BlockVizRequest, related_name="subcomponents")
+ 	transactions = models.TextField()
